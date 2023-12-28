78be5ef8fd983a6d00d88d647211c2394d6f69b1
Darwin Lantigua <darwinlf@outlook.com>
2023-12-27 21:57:40 -0400

    adding detail to library

--- libraries/views/library_views.py
+++ libraries/views/library_views.py
@@ -44,3 +44,8 @@ class UpdateView(generic.UpdateView):
             return render(self.request, self.template_name, {'form': form})
 
         return HttpResponseRedirect(reverse('libraries:library_index'))
+    
+class DetailView(generic.DetailView):
+    model = Library
+    template_name = 'library/library_detail.html'
+    context_object_name = 'library'
