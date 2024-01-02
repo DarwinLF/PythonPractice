91956067fc1456726a9b337b3275b1e079f223d8
Darwin Lantigua <darwinlf@outlook.com>
2024-01-01 22:11:29 -0400

    adding breadcrumbs

---
+++ utilities/utils.py
@@ -0,0 +1,18 @@
+from django.urls import resolve
+
+def get_breadcrumbs(request):
+    breadcrumbs = []
+    
+    # Add the homepage as the first breadcrumb
+    breadcrumbs.append({'name': 'Home', 'url': '/'})
+    
+    url_path = request.path
+    url_parts = url_path.split('/')
+    current_url = ''
+
+    for part in url_parts:
+        if part:
+            current_url += '/' + part
+            breadcrumbs.append({'name': part.capitalize(), 'url': current_url})
+
+    return breadcrumbs
