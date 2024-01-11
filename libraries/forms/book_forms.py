a74ace2dc350308149a4a569e0f70ad789dcf553
Darwin Lantigua <darwinlf@outlook.com>
2024-01-10 21:42:44 -0400

    adding gender to the templates of book

--- libraries/forms/book_forms.py
+++ libraries/forms/book_forms.py
@@ -8,10 +8,12 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'published_date', 'isbn', 'quantity',
-                   'rent_price', 'sale_price', 'author', 'library', 'status']
+                   'rent_price', 'sale_price', 'author', 'library', 'status',
+                   'gender']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'published_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'published_date': forms.DateInput(attrs={
+                'type': 'date', 'class': 'form-control'}),
             'isbn': forms.TextInput(attrs={'class': 'form-control'}),
             'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
             'rent_price': forms.NumberInput(attrs={'class': 'form-control'}),
@@ -19,6 +21,7 @@ class BookForm(forms.ModelForm):
             'author': forms.Select(attrs={'class': 'form-control'}),
             'library': forms.Select(attrs={'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-control'}),
+            'gender': forms.Select(attrs={'class': 'form-control'})
         }
 
     def clean(self):
@@ -31,7 +34,8 @@ class BookForm(forms.ModelForm):
             self.add_error('isbn', 'Invalid ISBN')
 
         if pub_date > timezone.now().date():
-            self.add_error('published_date', 'The published date can\'t be in the future')
+            self.add_error('published_date', 
+                           'The published date can\'t be in the future')
 
         isbn = isbn.replace('-', '')
 
