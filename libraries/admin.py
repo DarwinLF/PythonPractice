020fe8278c7ec8f20989ad2f0657a4cf2c548e55
Darwin Lantigua <darwinlf@outlook.com>
2024-01-16 19:54:08 -0400

    adding models to admin site

--- libraries/admin.py
+++ libraries/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import Library, Book, BookStatus, Rent
+from .models import Library, Book, BookStatus, BookGenders, Rent, RentStatus
 
 # Register your models here.
 
 admin.site.register(Library)
 admin.site.register(Book)
 admin.site.register(BookStatus)
+admin.site.register(BookGenders)
 admin.site.register(Rent)
+admin.site.register(RentStatus)
--- persons/admin.py
+++ persons/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Author, Customer, Employee
+from .models import Author, Customer, CustomerStatus, Employee
 
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Customer)
+admin.site.register(CustomerStatus)
 admin.site.register(Employee)
